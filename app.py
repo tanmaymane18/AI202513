3e001e817b82307b5387b44ae25e1aaa36e96d9f
Tanmay Mane <tanmaymane8@gmail.com>
2020-12-14 00:24:43 +0530

    default_hyperparas

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def get_params():
         lr = st.sidebar.checkbox(label='Logistic Regression')
         st.sidebar.header('Hyperparameter Tuning')
     else:
-        st.sidebar.header('Hyperparameter Tuning')
+        '''st.sidebar.header('Hyperparameter Tuning')
         svm_kernel = st.sidebar.selectbox(label='SVM Kernel', options=['rbf', 'linear', 'poly', 'sigmoid'])
         svm_c = st.sidebar.slider(label='SVM C', min_value=1.0, max_value=10.0)
         svm_param = (svm_kernel, svm_c)
@@ -56,7 +56,8 @@ def get_params():
         lr_param = (lr_solver, lr_c)
         model_params['lr'] = lr_param
         nb_alpha = st.sidebar.slider(label='NB Alpha', min_value=1.0, max_value=10.0)
-        model_params['nb'] = nb_alpha
+        model_params['nb'] = nb_alpha'''
+        pass
 
 
     if svm:
