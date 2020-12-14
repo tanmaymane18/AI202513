35b7471f2d6b833d26d9e90a58f9dba54989ffa0
Tanmay Mane <tanmaymane8@gmail.com>
2020-12-14 12:44:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -201,13 +201,14 @@ def load_data(params, fileName, train=True, default=True):
     #df = sklearn.utils.shuffle(df)
     #df.reset_index(inplace=True)
     #df.drop(columns=['index'], inplace=True)
-    st.dataframe(df.head())
-    st.write(df.describe())
+    if len(selectedCats)>1:
+        st.dataframe(df.head())
+        st.write(df.describe())
 
-    X, feature_vec = preprocess(df['body'], min_ngram, max_ngram, vec)
-    
-    Y = df['category'].astype('category').cat.codes
-    return train_test_split(X, Y, test_size=(100 - train_param) / 100, random_state=42, shuffle=True), feature_vec
+        X, feature_vec = preprocess(df['body'], min_ngram, max_ngram, vec)
+        
+        Y = df['category'].astype('category').cat.codes
+        return train_test_split(X, Y, test_size=(100 - train_param) / 100, random_state=42, shuffle=True), feature_vec
 
 #def load_new_data(fileName):
 
