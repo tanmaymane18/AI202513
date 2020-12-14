61d07a9c7aa2703258a7fa1ea36ab414be0e01ac
Tanmay Mane <tanmaymane8@gmail.com>
2020-12-14 23:57:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -259,7 +259,7 @@ def make_predictions(predictFile, clf, cat_codes, feature_vec):
         #st.text(content)
         body.append(content)
 
-    X=preprocess(pd.Series(body), test=True, feature_vec)
+    X=preprocess(pd.Series(body), test=True, vocab=feature_vec)
     Y = list(clf.predict(X))
     Y = list(map(lambda x: cat_codes[str(x)], Y))
     df = pd.DataFrame(data=list(zip(emails, body, Y)), columns=['email_name', 'body', 'Category'])
