57513e92e99ff3281cdd4c09551d740996ec03e6
Tanmay Mane <tanmaymane8@gmail.com>
2020-12-14 23:43:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -161,7 +161,7 @@ def preprocess(X, min_ngram=1, max_ngram=2, vec='Tf-Idf', test=False):
     st.write(X)
     st.write(vec)
     if vec == 'Tf-Idf':
-        cv = TfidfVectorizer(ngram_range=(min_ngram, max_ngram), max_features=1000)
+        cv = TfidfVectorizer(ngram_range=(min_ngram, max_ngram))
     elif vec == 'CountVector':
         cv = CountVectorizer(ngram_range=(min_ngram, max_ngram), max_features=1000)
 
