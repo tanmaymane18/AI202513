fbd1be119eb4e9a7bdef8dc78e558bc4e02dbde2
Tanmay Mane <tanmaymane8@gmail.com>
2020-12-14 12:45:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -184,7 +184,7 @@ def load_data(params, fileName, train=True, default=True):
     folder_name = fileName[:fileName.index('.')]
     cats = os.listdir('./'+folder_name)
     selectedCats = st.multiselect("Select Categories for Classification", cats)
-    st.text(selectedCats)
+    st.text('Selected Categories: ', *selectedCats)
     emails = []
     email_cat = []
     body = []
