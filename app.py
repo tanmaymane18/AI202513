c04807c25c66c2bc233a5dc3493b5a581bdd1556
Tanmay Mane <tanmaymane8@gmail.com>
2020-12-16 11:06:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -46,6 +46,8 @@ def get_params():
         #col1, col2 = st.sidebar.beta_columns(2)
         min_ngram = st.sidebar.number_input(label='min_ngrams',min_value=1, max_value=5)
         max_ngram = st.sidebar.number_input(label='max_ngrams', min_value=1, max_value=5)
+    else:
+        params = {'train_param': train_param, 'vectorization':'Tf-Idf', 'min_ngram':1, 'max_ngram':2, 'voting':'soft'}
 
 
     svm, nb, lr = False, False, False
