c9bb0757a5e11787335973e5a8aab701f1fbd3f3
Tanmay Mane <tanmaymane8@gmail.com>
2020-12-18 12:34:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -223,7 +223,7 @@ def load_data(params, fileName, train=True, default=True):
     if len(selectedCats)>1:
         st.markdown('**Loaded Data: **')
         st.dataframe(df.head())
-        st.info('**Basic Stats: **')
+        st.markdown('**Basic Stats: **')
         st.write(df.describe())
 
         X, feature_vec = preprocess(df['body'], min_ngram, max_ngram, vec)
