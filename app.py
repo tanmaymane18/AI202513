d729f81e9084e18da1e5334e65de6843a896a886
Tanmay Mane <tanmaymane8@gmail.com>
2020-12-18 12:53:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -231,7 +231,8 @@ def load_data(params, fileName, train=True, default=True):
         Y = df['category'].astype('category').cat.codes
         rt_cat_codes = dict(enumerate(df['category'].astype('category').cat.categories))
         cat_codes = json.dumps(rt_cat_codes)
-        st.write(pd.DataFrame(rt_cat_codes.items()))
+        st.markdown('**Basic Stats 📈: **')
+        st.write(pd.DataFrame(rt_cat_codes.items(), columns=['Encoding', 'Category']))
         #st.write(json.dumps(cat_codes))
         #cat_index = pd.Dataframe(dict(enumerate(df['category'].astype('category').cat.categories)))
         #st.write(cat_index)
